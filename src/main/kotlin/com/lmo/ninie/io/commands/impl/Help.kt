5b1ddcb9d551eeb86e4abb4ab24ceb1ee5493d40
Lionel <mottet.lionel@gmail.com>
2021-04-02 12:36:50 +0200

    rich help text

--- src/main/kotlin/com/lmo/ninie/io/commands/impl/Help.kt
+++ src/main/kotlin/com/lmo/ninie/io/commands/impl/Help.kt
@@ -9,15 +9,21 @@ import reactor.core.publisher.Mono
 
 class Help : CommandListener {
 
+    private val lineBreak = "\n"
+
     override fun matches(command: Command): Boolean = command.action == HELP
 
     override fun execute(command: Command): Mono<Message> =
             command.channel
-                    .flatMap { chan -> sendMessages(chan) }
+                    .flatMap { chan -> chan.createMessage("Ninie.IO is Helping ! :heart: \n help command") }
 
-    fun sendMessages(messageChannel : MessageChannel): Mono<Message>{
-        return messageChannel
-                .createMessage("Ninie.IO is Helping ! :heart: \n help command")
-    }
+    private fun buildMessage(): String =
+            """"
+                Ninie.IO is Helping ! :heart:
+                Commands :
+                $HELP - this command, for n00bs
+            """.trimMargin()
+
+
 
 }
