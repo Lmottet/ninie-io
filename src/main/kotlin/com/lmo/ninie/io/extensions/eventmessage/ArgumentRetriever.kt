69e58050b78e29cc60d2602da9e41997c1f3cfa5
Lionel <mottet.lionel@gmail.com>
2021-04-06 12:38:10 +0200

    use constant where possible

--- src/main/kotlin/com/lmo/ninie/io/extensions/eventmessage/ArgumentRetriever.kt
+++ src/main/kotlin/com/lmo/ninie/io/extensions/eventmessage/ArgumentRetriever.kt
@@ -1,10 +1,11 @@
 package com.lmo.ninie.io.extensions.eventmessage
 
+import com.lmo.ninie.io.constants.MagicStrings
 import discord4j.core.`object`.entity.Message
 import io.vavr.control.Option
 
 fun Message.extractArg(argumentNumber: Int): Option<String> {
-    val terms = this.content.split(" ")
+    val terms = this.content.split(MagicStrings.WHITESPACE)
     val args = terms.subList(2, terms.size)
     val argumentIndex = argumentNumber - 1
     return if (args.size > argumentIndex) {
--- src/main/kotlin/com/lmo/ninie/io/extensions/eventmessage/CommandRetriever.kt
+++ src/main/kotlin/com/lmo/ninie/io/extensions/eventmessage/CommandRetriever.kt
@@ -1,5 +1,6 @@
 package com.lmo.ninie.io.extensions.eventmessage
 
+import com.lmo.ninie.io.constants.MagicStrings
 import discord4j.core.`object`.entity.Message
 
-fun Message.extractAction() = this.content.split(" ")[1]
+fun Message.extractAction() = this.content.split(MagicStrings.WHITESPACE)[1]
