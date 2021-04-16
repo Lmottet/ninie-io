1c87242cacdb948b803614c6c8ab44d197bb4c09
Lionel <mottet.lionel@gmail.com>
2021-04-16 17:48:31 +0200

    trying stuff

--- src/main/kotlin/com/lmo/ninie/io/commands/impl/Fart.kt
+++ src/main/kotlin/com/lmo/ninie/io/commands/impl/Fart.kt
@@ -2,6 +2,7 @@ package com.lmo.ninie.io.commands.impl
 
 import com.lmo.ninie.io.commands.EmbedCommandBase
 import discord4j.core.`object`.entity.Message
+import discord4j.discordjson.json.EmbedImageData
 import discord4j.discordjson.json.ImmutableEmbedData
 import org.springframework.stereotype.Component
 
@@ -10,6 +11,7 @@ class Fart : EmbedCommandBase() {
 
     override fun response(message: Message) = ImmutableEmbedData
         .builder()
+        .image(EmbedImageData.builder().url("https://c.tenor.com/6ksAhluvauUAAAAM/fart-summer-vibe.gif").build())
         .url("https://c.tenor.com/6ksAhluvauUAAAAM/fart-summer-vibe.gif")
         .build()
 }
