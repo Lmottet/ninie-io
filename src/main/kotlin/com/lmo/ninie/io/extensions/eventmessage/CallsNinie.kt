23b3b16bcd09bfa3e466cfc2c4696dff34fdd592
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-06-26 21:19:04 +0200

    improve command detection

--- src/main/kotlin/com/lmo/ninie/io/extensions/eventmessage/CallsNinie.kt
+++ src/main/kotlin/com/lmo/ninie/io/extensions/eventmessage/CallsNinie.kt
@@ -3,5 +3,5 @@ package com.lmo.ninie.io.extensions.eventmessage
 import discord4j.core.`object`.entity.Message
 
 fun Message.callsNinie(prefix:String) :Boolean{
-    return !this.isBotAuthor() && this.content.startsWith(prefix)
+    return !this.isBotAuthor() && this.content.startsWith(prefix) && this.content.length>1
 }
