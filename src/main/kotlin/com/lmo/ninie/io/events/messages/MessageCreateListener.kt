9265e357e83ab946b6d21969ef5ef67520eb6426
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-06-26 20:32:14 +0200

    gpt-based improvements (kill .block usages where possible)

--- src/main/kotlin/com/lmo/ninie/io/events/messages/MessageCreateListener.kt
+++ src/main/kotlin/com/lmo/ninie/io/events/messages/MessageCreateListener.kt
@@ -16,32 +16,39 @@ import reactor.core.publisher.Mono
 
 @Service
 class MessageCreateListener(
-    val respondableMapperService: RespondableMapperService,
-    val aliasService: AliasService,
-    val botConfigurationProperties: BotConfigurationProperties
+    private val respondableMapperService: RespondableMapperService,
+    private val aliasService: AliasService,
+    private val botConfigurationProperties: BotConfigurationProperties
 ) : EventListener<MessageCreateEvent>() {
 
     override fun getEventType(): Class<MessageCreateEvent> = MessageCreateEvent::class.java
 
-    override fun execute(event: Mono<MessageCreateEvent>) = event.map { execute(it.message) }
-        .block()
-        ?.map { }
+    override fun execute(event: Mono<MessageCreateEvent>): Mono<*> {
+        return event.flatMap { messageCreateEvent ->
+            execute(messageCreateEvent.message)
+        }
+    }
 
-    private fun execute(message: Message): Mono<*> =
-        when {
-            message.callsNinie(botConfigurationProperties.prefix) -> executeCommand(message) ?: Mono.empty<Unit>()
+    private fun execute(message: Message): Mono<*> {
+        return when {
+            message.callsNinie(botConfigurationProperties.prefix) -> {
+                executeCommand(message) ?: Mono.empty<Unit>()
+            }
             !message.isBotAuthor() -> executeReaction(message)
             else -> Mono.empty<Unit>()
         }
+    }
 
-    private fun executeCommand(message: Message) =
-        aliasService.mapToCommand(findAlias(message))
+    private fun executeCommand(message: Message): Mono<MessageData>? {
+        return aliasService.mapToCommand(findAlias(message))
             .respondTo(message)
+    }
 
-    private fun executeReaction(message: Message): Mono<MessageData> =
-        respondableMapperService.reactToCreation(message)
+    private fun executeReaction(message: Message): Mono<MessageData> {
+        return respondableMapperService.reactToCreation(message)
+    }
 
-    private fun findAlias(message: Message): Alias =
-        aliasService.find(message.extractCommandAlias()) ?: Alias.Unknown
-
+    private fun findAlias(message: Message): Alias {
+        return aliasService.find(message.extractCommandAlias()) ?: Alias.Unknown
+    }
 }
