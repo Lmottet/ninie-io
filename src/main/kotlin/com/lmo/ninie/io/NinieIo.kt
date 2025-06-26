cce05d9f2f6d3149f6896be117846e264bce005e
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-06-26 21:43:00 +0200

    sanity check

--- src/main/kotlin/com/lmo/ninie/io/NinieIo.kt
+++ src/main/kotlin/com/lmo/ninie/io/NinieIo.kt
@@ -12,5 +12,7 @@ fun main(args: Array<String>) {
     val port = System.getenv("PORT") ?: "8080"
     System.setProperty("server.port", port)
     System.setProperty("server.address", "0.0.0.0")
+    println("Listening on ${System.getProperty("server.address")}:${System.getProperty("server.port")}")
+
     runApplication<NinieIo>(*args)
 }
