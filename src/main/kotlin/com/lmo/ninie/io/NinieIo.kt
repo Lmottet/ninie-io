adbfb8f499e323f9ca1358ea109d8b1d9de5025e
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-06-27 11:55:35 +0200

    force conf

--- src/main/kotlin/com/lmo/ninie/io/NinieIo.kt
+++ src/main/kotlin/com/lmo/ninie/io/NinieIo.kt
@@ -3,12 +3,16 @@ package com.lmo.ninie.io
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class NinieIo
 
 fun main(args: Array<String>) {
+    val port = System.getenv("PORT") ?: "10000"
+    System.setProperty("server.port", port)
+    System.setProperty("server.address", "0.0.0.0")
+    println("Listening on ${System.getProperty("server.address")}:${System.getProperty("server.port")}")
+
     runApplication<NinieIo>(*args)
 }
