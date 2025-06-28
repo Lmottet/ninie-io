1357529bdf6cafb73b59b29352fb349a0920ed54
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-06-28 13:19:50 +0200

    prevent self reacts

--- src/main/kotlin/com/lmo/ninie/io/interactions/RespondableMapperServiceImpl.kt
+++ src/main/kotlin/com/lmo/ninie/io/interactions/RespondableMapperServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.lmo.ninie.io.interactions
 
 import com.lmo.ninie.io.constants.text.Emojis
+import com.lmo.ninie.io.extensions.eventmessage.isBotAuthor
 import com.lmo.ninie.io.interactions.reactions.*
 import discord4j.core.`object`.entity.Message
 import discord4j.core.`object`.reaction.ReactionEmoji
@@ -23,6 +24,12 @@ class RespondableMapperServiceImpl(
             .switchIfEmpty(Mono.defer { randomResponseService.respondTo(message) })
     }
 
-    override fun reactToUpdate(message: Message): Mono<Unit> = message.addReaction(ReactionEmoji.unicode(Emojis.EYES)).map { }
+    override fun reactToUpdate(message: Message): Mono<Unit> {
+        if (message.isBotAuthor())
+            return Mono.empty()
+
+        val map = message.addReaction(ReactionEmoji.unicode(Emojis.EYES)).map { }
+        return map
+    }
 
 }
