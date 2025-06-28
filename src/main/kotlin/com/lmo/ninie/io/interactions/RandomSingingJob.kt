becce829d2820f9e0979a74506745a09acd6c38d
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-06-28 13:13:31 +0200

    random singing, hello on boot

---
+++ src/main/kotlin/com/lmo/ninie/io/interactions/RandomSingingJob.kt
@@ -0,0 +1,46 @@
+﻿package com.lmo.ninie.io.interactions
+
+import com.lmo.ninie.io.dao.SongDao
+import discord4j.common.util.Snowflake
+import discord4j.core.GatewayDiscordClient
+import discord4j.core.`object`.entity.channel.TextChannel
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.ThreadLocalRandom
+
+@Component
+class RandomSingingJob(private val scheduler: TaskScheduler, private val client: GatewayDiscordClient, private val songDao: SongDao) : ApplicationRunner {
+
+    private val random = ThreadLocalRandom.current()
+    private val channelId = Snowflake.of("826184542393204828")
+
+    override fun run(args: ApplicationArguments?) {
+        client.getChannelById(channelId)
+            .ofType(TextChannel::class.java)
+            .flatMap { channel -> channel.createMessage("Ninie woke up !") }
+            .subscribe()
+
+        scheduleNext()
+    }
+
+    private fun scheduleNext() {
+        val minMillis = Duration.ofHours(24).toMillis()
+        val delay = minMillis + random.nextLong(Duration.ofHours(72).toMillis() - minMillis)
+
+        val nextInstant = Instant.now().plusMillis(delay)
+        scheduler.schedule({ executeTask() }, nextInstant)
+    }
+
+    private fun executeTask() {
+        client.getChannelById(channelId)
+            .ofType(TextChannel::class.java)
+            .flatMap { channel -> channel.createMessage(songDao.any().content) }
+            .subscribe()
+
+        scheduleNext()
+    }
+}
