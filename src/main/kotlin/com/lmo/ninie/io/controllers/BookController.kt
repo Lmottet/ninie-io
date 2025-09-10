dec02155045c06e44caada5d041464b22b6ca73d
Lionel Mottet | Technology Partner <lionel.mottet@technologypartner.io>
2025-09-10 22:06:40 +0200

    add endpoints

--- src/main/kotlin/com/lmo/ninie/io/controllers/BookController.kt
+++ src/main/kotlin/com/lmo/ninie/io/controllers/BookController.kt
@@ -1,9 +1,13 @@
 ﻿package com.lmo.ninie.io.controllers
 
+import com.lmo.ninie.io.dto.request.CreateBookRequest
 import com.lmo.ninie.io.dto.response.BookResponse
 import com.lmo.ninie.io.dto.response.toResponse
 import com.lmo.ninie.io.services.BookService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,4 +17,10 @@ class BookController(val bookService: BookService) {
 
     @GetMapping
     fun fetch(): Set<BookResponse> = bookService.list().map { it.toResponse() }.toSet()
+
+    @GetMapping("/{id}")
+    fun getAuthor(@PathVariable id: Long): BookResponse = bookService.get(id).toResponse()
+
+    @PostMapping
+    fun create(@RequestBody request: CreateBookRequest): BookResponse = bookService.create(request).toResponse()
 }
--- src/main/kotlin/com/lmo/ninie/io/services/BookService.kt
+++ src/main/kotlin/com/lmo/ninie/io/services/BookService.kt
@@ -1,6 +1,7 @@
 ﻿package com.lmo.ninie.io.services
 
 import com.lmo.ninie.io.data.entities.Book
+import com.lmo.ninie.io.data.entities.Series
 import com.lmo.ninie.io.data.repositories.BookRepository
 import com.lmo.ninie.io.dto.request.CreateBookRequest
 import org.springframework.stereotype.Service
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service
 class BookService(private val bookRepository: BookRepository) {
 
     fun list(): List<Book> = bookRepository.findAll()
+    fun get(id: Long): Book = bookRepository.findById(id).orElseThrow()
 
     fun create(request: CreateBookRequest): Book = bookRepository.save(Book(title = request.title, tome = request.tome))
 }
